d5a5cb655cc71e6833c258e6a38369f57b93014a
Ricardo Lima Caratti <ricardo.caratti@adm.cogect>
2020-09-06 11:44:40 -0300

    START

---
+++ MSI001.h
@@ -0,0 +1,10 @@
+/**
+ * @file MSI001.h
+ * @author Ricardo Lima Caratti (pu2clr@gmaol.com)
+ * @brief Library under construction....
+ * @version 0.1
+ * @date 2020-09-06
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
